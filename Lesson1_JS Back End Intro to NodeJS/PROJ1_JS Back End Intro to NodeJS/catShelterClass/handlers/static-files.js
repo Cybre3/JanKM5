a05bd97757566a4a26ba09502de060686ee042da
Cybre3 <71160954+Cybre3@users.noreply.github.com>
2021-07-28 23:12:17 -0400

    all forms rendered, add-cat and add-breed GET and POST working

--- Lesson1_JS Back End Intro to NodeJS/PROJ1_JS Back End Intro to NodeJS/catShelterClass/handlers/static-files.js
+++ Lesson1_JS Back End Intro to NodeJS/PROJ1_JS Back End Intro to NodeJS/catShelterClass/handlers/static-files.js
@@ -6,13 +6,17 @@ function getContentType(url) {
   if (url.endsWith("css")) {
     return "text/css";
   } else if (url.endsWith("html")) {
-    return "text/html";
+    return "html";
   } else if (url.endsWith("png")) {
-    return "text/png";
+    return "img";
   } else if (url.endsWith("js")) {
-    return "text/js";
+    return "js";
   } else if (url.endsWith('ico')) {
-      return 'text/ico';
+    return 'ico';
+  } else if (url.endsWith('jpg')) {
+    return 'jpg';
+  } else if (url.endsWith('jpeg')) {
+    return 'jpeg';
   }
 }
 
@@ -23,27 +27,54 @@ module.exports = (req, res) => {
   const pathname = url.parse(req.url).pathname;
 
   if (pathname.startsWith("/content") && req.method === "GET") {
-    
-    fs.readFile(`./${pathname}`, "utf-8", (err, data) => {
-      if (err) {
-        console.log(err);
-        res.writeHead(404, {
-          "Content-Type": "text/plain",
+
+    if(pathname.endsWith('png') || pathname.endsWith('jpg') || pathname.endsWith('jpeg') || pathname.endsWith('ico') && req.method === 'GET') {
+
+      fs.readFile(`./${pathname}`, (err, data) => {
+        if (err) {
+          console.log(err);
+          res.writeHead(404, {
+            "Content-Type": "text/plain"
+          });
+  
+          res.write("Error was found");
+          res.end();
+          return;
+        }
+  
+  
+        console.log(pathname);
+        res.writeHead(200, {
+          "Content-Type": getContentType(pathname)
         });
-
-        res.write("Error was found");
+        res.write(data);
         res.end();
-        return;
-      }
-
-
-      console.log(pathname);
-      res.writeHead(200, {
-        "Content-Type": getContentType(pathname),
       });
-      res.write(data);
-      res.end();
-    });
+
+    } else {
+      
+      fs.readFile(`./${pathname}`, "utf-8", (err, data) => {
+        if (err) {
+          console.log(err);
+          res.writeHead(404, {
+            "Content-Type": "text/plain"
+          });
+  
+          res.write("Error was found");
+          res.end();
+          return;
+        }
+  
+  
+        console.log(pathname);
+        res.writeHead(200, {
+          "Content-Type": getContentType(pathname)
+        });
+        res.write(data);
+        res.end();
+      });
+    }
+    
   } else {
     return true;
   }
